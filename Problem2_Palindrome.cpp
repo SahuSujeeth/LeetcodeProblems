94dff9c7b937042300027de7ea5ff810ab4b52eb
Sahu Sujeeth <sahusujeeth2004@gmail.com>
2025-08-29 23:12:56 +0530

    Added Problem 2: Palindrome Number solution

---
+++ Problem2_Palindrome.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int x) {
+    if (x < 0) return false;
+
+    long long reversed = 0;
+    int original = x;
+
+    while (x > 0) {
+        int digit = x % 10;
+        reversed = reversed * 10 + digit;
+        x /= 10;
+    }
+
+    return original == reversed;
+}
+
+int main() {
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
+
+    if (isPalindrome(n))
+        cout << "Palindrome\n";
+    else
+        cout << "Not Palindrome\n";
+
+    return 0;
+}
