5823125cb80897415b7fde1820e4b44e31b8d7ba
Sahu Sujeeth <sahusujeeth2004@gmail.com>
2025-09-20 22:32:29 +0530

    new problem added

---
+++ Problem13_Checking_Sorted_Array.cpp
@@ -0,0 +1,35 @@
+// Check if Array Is Sorted and Rotated
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+            if (count > 1) return false;
+        }
+
+        return true;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    
+    vector<int> nums1 = {3, 4, 5, 1, 2};
+    vector<int> nums2 = {2, 1, 3, 4};
+    vector<int> nums3 = {1, 2, 3};
+
+    cout << (sol.check(nums1) ? "true" : "false") << endl;  
+    cout << (sol.check(nums2) ? "true" : "false") << endl;  
+    cout << (sol.check(nums3) ? "true" : "false") << endl;  
+
+    return 0;
+}
